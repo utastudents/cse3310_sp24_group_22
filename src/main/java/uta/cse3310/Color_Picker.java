07547d6a3c70f918e435f594d4a8d9c8af0ec477
vtt3336 <vtt3336@mavs.uta.edu>
2024-03-31 15:35:47 -0500

    added function to color picker

--- src/main/java/uta/cse3310/Color_Picker.java
+++ src/main/java/uta/cse3310/Color_Picker.java
@@ -3,4 +3,8 @@ import java.util.ArrayList;
 public class Color_Picker {
     ArrayList<String> Color_presets = new ArrayList<String>();
     ArrayList<String> Color_picked = new ArrayList<String>();
+    
+    public void assign_colors(){
+        
+    }
 }
