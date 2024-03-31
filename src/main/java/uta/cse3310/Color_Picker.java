d789ef5206d6eebd62f9d09ae28632cd26000f46
vtt3336 <vtt3336@mavs.uta.edu>
2024-03-31 15:28:21 -0500

    Added Color picker class

---
+++ src/main/java/uta/cse3310/Color_Picker.java
@@ -0,0 +1,6 @@
+package uta.cse3310;
+import java.util.ArrayList;
+public class Color_Picker {
+    ArrayList<String> Color_presets = new ArrayList<String>();
+    ArrayList<String> Color_picked = new ArrayList<String>();
+}
