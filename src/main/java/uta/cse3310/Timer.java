f0de26473b092aea0b9ad3cb2fd9a5e3c340865c
Akito Kishi <akito2155@gmail.com>
2024-04-14 19:17:33 -0500

    Messaging and Timer 4/14

---
+++ src/main/java/uta/cse3310/Timer.java
@@ -0,0 +1,59 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Timer {
+    private Timer timer;
+    private int secondsRemaining;
+    private boolean isGameEnded;
+    private int playerScore;
+
+    public Timer(int seconds, int playerScore) {
+        this.secondsRemaining = seconds;
+        this.timer = new Timer();
+        this.isGameEnded = false;
+        this.playerScore = playerScore;
+    }
+
+    public void start() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (secondsRemaining > 0) {
+                    System.out.println("Time remaining: " + secondsRemaining + " seconds");
+                    secondsRemaining--;
+                } else {
+                    System.out.println("Time's up!");
+                    timer.cancel();
+                    isGameEnded = true;
+                    declareWinner();
+                }
+            }
+        }, 0, 1000); // 1000 milliseconds = 1 second
+    }
+
+    public void stop() {
+        timer.cancel();
+        System.out.println("Timer stopped.");
+    }
+
+    // Method to prompt players for end game actions
+    private void promptPlayerForEndGame() {
+        // Add code here to prompt players for end game actions
+        System.out.println("Prompting players for end game actions...");
+    }
+
+    public boolean isGameEnded() {
+        return isGameEnded;
+    }
+
+    // Method to declare the winner with the highest score
+    private void declareWinner() {
+        // Add code here to declare the winner with the highest score
+        System.out.println("The winner is the player with the highest score: " + playerScore);
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer(60, 10); // 60 seconds, player score: 10
+        timer.start();
+    }
+}
