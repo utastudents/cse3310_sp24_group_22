032d13684f78f1f3d85c8be83b1bb4dfe6def04b
vincenttrinh3336 <158768889+vincenttrinh3336@users.noreply.github.com>
2024-04-29 21:13:41 +0000

    trying new method

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -5,31 +5,54 @@ import java.util.*;
 
 public class Leaderboard {
     public TreeMap<String, Integer> LB;
-    
+	public Map sortedScore;
+
+	public static <K, V extends Comparable<V> > Map<K, V> sortScores(final Map<K, V> map) 
+	{  
+		Comparator<K> valueComparator = new Comparator<K>() 
+		{ 
+			public int compare(K k1, K k2) 
+			{ 
+				int comp = map.get(k1).compareTo(map.get(k2)); 
+				if (comp == 0) 
+					return 1; 
+				else
+					return comp; 
+			} 
+		}; 
+		Map<K, V> sorted = new TreeMap<K, V>(valueComparator); 
+		sorted.putAll(map); 
+		return sorted; 
+	} 
 
     public Leaderboard() {
         // Initialize the TreeMap with a custom comparator to sort by score in descending order
         LB = new TreeMap<>();
-        public ArrayList<Integer> valueList = new ArrayList<Integer>(LB.values());
-        public ArrayList<String> keyList = new ArrayList<String>(LB.keySet());
+		
     }
-
     // Method to add a score to the leaderboard
     public void add(String handle, int score) {
         LB.put(handle, score);
+		sortedScore.clear();
+        sortedScore = sortScores(LB);
     }
     
     public void update(String handle, int score)
     {
     	int new_score = score + LB.get(handle);
-    	LB.put(handle, new_score);
+        LB.put(handle, new_score)
+		sortedScore.clear();
+        sortedScore = sortScores(LB);
+    }
     }
     
     public void remove(String handle)
     {
     	if (handle!= null)
     	{
-    		LB.remove(handle);
+            LB.remove(handle);
+			sortedScore.clear();
+        	sortedScore = sortScores(LB);
     	}
     }
 }
