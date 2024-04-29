a36222a37ecaf0e220552a5f45b5304ab502ba10
vincenttrinh3336 <158768889+vincenttrinh3336@users.noreply.github.com>
2024-04-29 16:10:12 +0000

    try to fix order of leaderboard

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,6 +1,7 @@
 package uta.cse3310;
 import java.util.Comparator;
 import java.util.TreeMap;
+import java.util.Comparator;
 
 public class Leaderboard {
     public TreeMap<String, Integer> LB;
@@ -8,17 +9,32 @@ public class Leaderboard {
     public Leaderboard() {
         // Initialize the TreeMap with a custom comparator to sort by score in descending order
         LB = new TreeMap<>();
+        Comparator<K> compareScore =new Comparator<K> (){
+            public int compare(K k1, K k2) 
+                  { 
+                      int comp = map.get(k1).compareTo( 
+                          map.get(k2)); 
+                      if (comp == 0) 
+                          return 1; 
+                      else
+                          return comp; 
+                  } 
+        }
+        Map<K, V> sortedScore = new TreeMap<K, V>(compareScore); 
+
     }
 
     // Method to add a score to the leaderboard
     public void add(String handle, int score) {
         LB.put(handle, score);
+        sortedScore.putAll(map);
     }
     
     public void update(String handle, int score)
     {
     	int new_score = score + LB.get(handle);
     	LB.put(handle, new_score);
+        sortedScore.putAll(map);
     }
     
     public void remove(String handle)
