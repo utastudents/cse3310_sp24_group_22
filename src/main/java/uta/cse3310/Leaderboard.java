f189ebca8acd3df0d1d1b4d298d93dc5a20cbabc
vincenttrinh3336 <158768889+vincenttrinh3336@users.noreply.github.com>
2024-04-29 21:47:39 +0000

    trying new method

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -7,7 +7,7 @@ public class Leaderboard {
     public TreeMap<String, Integer> LB;
 	public SortedSet sortedScore;
 
-	static <K,V> SortedSet<Map.Entry<K,V>> sortingScore(Map<K,V> map) {
+	static <K,V> extends Comparable<? super V>SortedSet<Map.Entry<K,V>> sortingScore(Map<K,V> map) {
         SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
             new Comparator<Map.Entry<K,V>>() {
                 @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
