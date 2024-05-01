6c619f00ef0a65987ad67c1784c07bf88e1372fb
Samuel Mathew Vennalil <142947022+samuelmv30@users.noreply.github.com>
2024-05-01 15:17:49 -0500

    Add files via upload

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,38 +1,33 @@
 package uta.cse3310;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.net.URISyntaxException;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void test_valid_player() throws URISyntaxException, InterruptedException {
+        // Create a sample UserEvent object
+        UserEvent U = new UserEvent();
+        U.Handle = "this handle is too long";
+        U.ready = -2;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        // Convert the UserEvent object to JSON string
+        Gson gson = new GsonBuilder().create();
+        String jsonString = gson.toJson(U);
+
+        // Create an instance of AppClient
+        AppClient ac = new AppClient();
+
+        // Simulate receiving the message from the server
+        String message = ac.onMessage(jsonString);
+
+        // Check if the received message indicates disapproval
+        assertEquals("disapproved", message);
     }
 }
+
